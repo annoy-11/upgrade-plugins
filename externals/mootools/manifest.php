eb979fbb885969ad722ae8668656e283ef076f3a
root <root@ip-172-26-4-128.us-west-2.compute.internal>
2020-08-10 04:08:39 +0000

    SE commited

---
+++ externals/mootools/manifest.php
@@ -0,0 +1,15 @@
+<?php return array(
+  'package' => array(
+    'type' => 'external',
+    'name' => 'mootools',
+    'version' => '5.2.1',
+    'revision' => '$Revision: 9747 $',
+    'path' => 'externals/mootools',
+    'repository' => 'socialengine.com',
+    'title' => 'Mootools',
+    'author' => 'Webligo Developments',
+    'directories' => array(
+      'externals/mootools',
+    )
+  )
+) ?>
