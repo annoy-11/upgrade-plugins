a239acbd3d9237c1f160e2d42210b69f6701cda3
root <root@ip-172-31-32-149.ap-south-1.compute.internal>
2020-08-17 14:13:48 +0000

    added new script file

--- script.py
+++ script.py
@@ -3,28 +3,85 @@ import subprocess as sp
 plugin_name = input("Please enter plugin name")
 
 
-all_demo=[['fb_clone',['Otpsms', 'Sesshortcut', 'Sesarticle', 'Seserrorpage','Sesevent','Seseventvideo','Sesalbum', 'Sespoke','Sesadvsitenotification', '']], ['utube_clone',['activity','testapi']]]
+all_demo=[['fb_clone',['otpsms', 'sesshortcut', 'sesarticle', 'seserrorpage','sesevent','seseventvideo','sesalbum', 'sespoke','sesadvsitenotification']],
+          ['utube_clone',['activity','testapi']],
+          ['instaclone_demo', ['sesbasic','sesalbum','sesvideo','sesfixedlayout','sesminify','sesdemouser','sesblog','eandstories','sesadvancedactivity','sesbasic']]
+          ]
+
+#check out to plugins_tar file
+os.system("git checkout plugins_tarfile")
+
+# copy the tar file from plugins_tarfile to local
+os.system("cp module-{}* /var/www/html".format(plugin_name))
+
+
 
 
 for i in range(len(all_demo)):
 
-	if plugin_name in all_demo[i][1]:
+    if plugin_name in all_demo[i][1]:
 
-		sp.run("git checkout {}".format(all_demo[i][0]))
-		
-                 
-		print("Plugin {} exist in {} demo".format(plugin_name, all_demo[i][0]))
-		#sp.getoutput("cp that tar file to branch where to upgrade plugin repo")
-		#git show <branch_name>:path/to/file >path/to/local/file
-		#sp.getoutput("delete plugin package rm -rf path/to/plugin/module_name*")
+        #checkout to branch where we found plugin that we updated
+        os.system("git checkout {}".format(all_demo[i][0]))
 
-		#untar module to current repo
-		# git add for adding files changes
-		# git commit -m "plugin_name upgrade successful"
-		# git push 
+        #removed old package file
+        os.system("git rm -rf application/packages/module-{}*".format(plugin_name))
 
-	else:
+        # extract tar file to branch
+        os.system("tar -xvf /var/www/html/module-{}* -C /var/www/html/upgrade-plugins".format(plugin_name))
 
-		print("Plugin {} does not exist in {} demo".format(plugin_name, all_demo[i][0]))
+        # gave permission to package folder 
+        os.system("chmod -R 777 application/packages")
 
+        # delete tar file from the local
+        os.system("rm -rf /var/www/html/module-{}*".format(plugin_name)) 
+
+        
+        #untar module to current repo
+        # git add for adding files changes
+        # git commit -m "plugin_name upgrade successful"
+        # git push 
+import os
+import subprocess as sp
+plugin_name = input("Please enter plugin name")
+
+
+all_demo=[['fb_clone',['otpsms', 'sesshortcut', 'sesarticle', 'seserrorpage','sesevent','seseventvideo','sesalbum', 'sespoke','sesadvsitenotification']],
+          ['utube_clone',['activity','testapi']],
+          ['instaclone_demo', ['sesbasic','sesalbum','sesvideo','sesfixedlayout','sesminify','sesdemouser','sesblog','eandstories','sesadvancedactivity','sesbasic']]
+          ]
+
+#check out to plugins_tar file
+os.system("git checkout plugins_tarfile")
+
+# copy the tar file from plugins_tarfile to local
+os.system("cp module-{}* /var/www/html".format(plugin_name))
+
+
+
+
+for i in range(len(all_demo)):
+
+    if plugin_name in all_demo[i][1]:
+
+        #checkout to branch where we found plugin that we updated
+        os.system("git checkout {}".format(all_demo[i][0]))
+
+        #removed old package file
+        os.system("git rm -rf application/packages/module-{}*".format(plugin_name))
+
+        # extract tar file to branch
+        os.system("tar -xvf /var/www/html/module-{}* -C /var/www/html/upgrade-plugins".format(plugin_name))
+
+        # gave permission to package folder 
+        os.system("chmod -R 777 application/packages")
+
+        # delete tar file from the local
+        os.system("rm -rf /var/www/html/module-{}*".format(plugin_name)) 
+
+        
+        #untar module to current repo
+        # git add for adding files changes
+        # git commit -m "plugin_name upgrade successful"
+        # git push 
 
