130bc57eb04c3f1d768c5a7e7af79ca5c270cfa1
root <root@ip-172-31-32-149.ap-south-1.compute.internal>
2020-08-17 11:00:12 +0000

    added permission

--- script.py
+++ script.py
@@ -11,8 +11,8 @@ for i in range(len(all_demo)):
 	if plugin_name in all_demo[i][1]:
 
 		sp.run("git checkout {}".format(all_demo[i][0]))
-		sp.run("touch {}".format(all_demo[i][0]))
-		 
+		
+                 
 		print("Plugin {} exist in {} demo".format(plugin_name, all_demo[i][0]))
 		#sp.getoutput("cp that tar file to branch where to upgrade plugin repo")
 		#git show <branch_name>:path/to/file >path/to/local/file
